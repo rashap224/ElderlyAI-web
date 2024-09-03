9066efd405cc2448c95d15a57d0fb4ad30dd746f
Mohammad Faizan <faizancurious@gmail.com>
2024-09-03 22:09:15 +0530

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ import { MagicButton } from "./ui/magicbuton";
 export default function Navbar() {
   return (
     <div className="sticky top-0 z-50 flex h-16 w-full items-center justify-center border-b bg-transparent backdrop-blur-md transition-all">
-      <div className="flex h-full w-full px-2 md:w-[85%] md:px-0 lg:w-[60%]">
+      <div className="flex h-full w-full px-2 md:w-[85%] md:px-0 lg:w-[55%]">
         <div className="my-1 flex h-full w-full items-center justify-between md:my-0">
           {/* Logo */}
           <div className="flex items-center gap-2">
@@ -26,20 +26,6 @@ export default function Navbar() {
             </Link>
           </div>
           {/* Navlinks */}
-          <div className="hidden gap-1 lg:flex">
-            <Link href={"/"}>
-              <Button variant={"ghost"}>Home</Button>
-            </Link>
-            <Link href={"/#pricing"}>
-              <Button variant={"ghost"}>Pricing</Button>
-            </Link>
-            <Link href={"/#contact"}>
-              <Button variant={"ghost"}>Contact</Button>
-            </Link>
-            <Link href={"/#faq"}>
-              <Button variant={"ghost"}>FAQs</Button>
-            </Link>
-          </div>
           {/* Getstarted btn */}
           <div className="flex items-center gap-3">
             <Link
